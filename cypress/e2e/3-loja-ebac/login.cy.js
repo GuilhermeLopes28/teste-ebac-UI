7121a9bd0159bd6243467218ca281400ab740f2b
Guilherme Lopes Silva Do Nascimento <gnascimento@eurobraslogistica.com.br>
2024-11-22 14:53:42 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,12 +2,34 @@
 
 describe('funcionalidade: login', () =>{
 
-    it('deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('deve fazer login com sucesso', () =>{
         cy.get('#username').type('guilherme.teste.@teste.com.br')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, guilherme.teste. (não é guilherme.teste.? Sair)')
         
     })
+
+    it('deve exibir uma mensagem de erro ao inserir um usuário inválido', () => {
+        cy.get('#username').type('guilhermeteste.@teste.com.br')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        //cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. ')
+        cy.get('.woocommerce-error > li').should('exist')
+    })
+
+    it('deve exibir uma mensagme de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('guilherme.teste.@teste.com.br')
+        cy.get('#password').type('teste.123')
+        cy.get('.woocommerce-form > .button').click()
+        //cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. ')
+        cy.get('.woocommerce-error > li').should('exist')
+    });
 })
