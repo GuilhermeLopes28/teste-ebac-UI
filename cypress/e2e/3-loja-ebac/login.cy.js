13d8999bd14e68b5799c6f1cdf23a0055cf7be7b
Guilherme Lopes Silva Do Nascimento <gnascimento@eurobraslogistica.com.br>
2024-11-27 10:11:23 -0300

    arquivo de dados e fixtures

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('funcionalidade: login', () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
     //afterEach(() => {
         //cy.screenshot()
@@ -25,11 +26,28 @@ describe('funcionalidade: login', () =>{
         cy.get('.woocommerce-error > li').should('exist')
     })
 
-    it('deve exibir uma mensagme de erro ao inserir senha inválida', () => {
+    it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('guilherme.teste.@teste.com.br')
         cy.get('#password').type('teste.123')
         cy.get('.woocommerce-form > .button').click()
         //cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. ')
         cy.get('.woocommerce-error > li').should('exist')
+    });
+
+    it('deve fazer login com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        //cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, guilherme.teste. (não é guilherme.teste.? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+    });
+    it.only('deve fazer login com sucesso usando fixture', () => {
+        cy.fixture('perfil').then(dados =>{
+            cy.get('#username').type(dados.usuario, {log: false})
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            //cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, guilherme.teste. (não é guilherme.teste.? Sair)')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        })
     });
 })
